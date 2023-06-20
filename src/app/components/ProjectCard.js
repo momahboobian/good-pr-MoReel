3cd1ef90300f98cb20e3f40690a4c75abfe23544
Mo Mahboobian <momahboobian@gmail.com>
2023-06-20 13:02:36 +0100

    Creating Project Card

---
+++ src/app/components/ProjectCard.js
@@ -0,0 +1,18 @@
+const ProjectCard = () => {
+  return (
+    <div className="flex flex-col gap-6 pt-10">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <div className="flex flex-col justify-between h-[250px] bg-[#1A1E1F] rounded-xl p-4">
+            <h1 className="font-bold text-sm">Project</h1>
+            <button className="bg-[#37BCBA] text-black rounded-lg p-2 pl-6 pr-6">
+              See All Project
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/app/components/TeamOverviewHeader.js
+++ src/app/components/TeamOverviewHeader.js
@@ -1,3 +1,5 @@
+import ProjectCard from "./ProjectCard";
+
 const TeamOverview = () => {
   return (
     <div className="flex-1 w-screen bg-[#070E0E] p-6">
@@ -6,6 +8,7 @@ const TeamOverview = () => {
       <p className="font-light	text-xs	text-gray-600	pt-2">
         Track you projects, tasks & team activity here
       </p>
+      <ProjectCard />
     </div>
   );
 };
