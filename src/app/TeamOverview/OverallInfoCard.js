824bf9730e3acf29423ca2c05221b121c08eaa5c
nataliizab <nataliia.zab@gmail.com>
2023-06-20 22:25:16 +0100

    Update OverallInfoCard.js

--- src/app/TeamOverview/OverallInfoCard.js
+++ src/app/TeamOverview/OverallInfoCard.js
@@ -10,37 +10,37 @@ export default function OverallInfoCard() {
         <button className="bg-[#111827] text-[#F9F9F9]">...</button>
       </div>
       <div className="flex flex-column justify-left gap-1 items-center my-4">
-        <div className="text-[#F9F9F9] font-bold text-xl">67</div>
+        <div className="text-[#F9F9F9] font-bold text-2xl">67</div>
         <div className="text-[#F9F9F9] ">|</div>
-        <div className="text-[#F9F9F9] font-bold text-xl">105</div>
-        <p className="text-[#606467] text-xs ">Tasks Done</p>
+        <div className="text-[#F9F9F9] font-bold text-2xl">105</div>
+        <p className="text-[#606467] text-xs ml-[10px] items-end">Tasks Done</p>
       </div>
       <div className="flex flex-column justify-between  mt-2">
-        <button className="w-24 bg-[#E2E949] rounded-xl p-3">
-          <div className="w-20 ">
-            <div className="flex justify-center">
+        <button className="w-20 bg-[#E2E949] rounded-xl p-3">
+          <div className="w-16 ">
+            <div className="flex justify-center mt-2">
               <FaShapes />
             </div>
-            <p className="font-bold">28</p>
-            <p className="text-xs ">Tasks</p>
+            <p className="font-bold text-lg mt-2">28</p>
+            <p className="text-xs mt-2">Tasks</p>
           </div>
         </button>
-        <button className="w-24 bg-[#36BCBA] rounded-xl p-3">
-          <div className="w-20">
-            <div className="flex justify-center">
+        <button className="w-20 bg-[#36BCBA] rounded-xl p-3">
+          <div className="w-16">
+            <div className="flex justify-center mt-2">
               <IoSync />
             </div>
-            <p className="font-bold">17</p>
-            <p className="text-xs ">In progress</p>
+            <p className="font-bold text-lg mt-2">17</p>
+            <p className="text-xs mt-2">In progress</p>
           </div>
         </button>
-        <button className="w-24 bg-[#F55706] rounded-xl p-3">
-          <div className="w-20">
-            <div className="flex justify-center">
+        <button className="w-20 bg-[#F55706] rounded-xl p-3">
+          <div className="w-16">
+            <div className="flex justify-center mt-2">
               <FaExternalLinkAlt />
             </div>
-            <p className="font-bold">11</p>
-            <p className="text-xs">Completed</p>
+            <p className="font-bold text-lg mt-2">11</p>
+            <p className="text-xs mt-2">Completed</p>
           </div>
         </button>
       </div>
