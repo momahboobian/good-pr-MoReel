b06023b0e633d3ae6aba0a415a0e88ce7f2b6c00
Lorena <lorenacapraru@gmail.com>
2023-06-21 10:38:41 +0100

    Text alignment

--- src/app/components/TasksActivity.js
+++ src/app/components/TasksActivity.js
@@ -7,38 +7,44 @@ const TaskActivity = () => {
   return (
     <div className="text-white">
       <h2>Task Activity</h2>
-      <table>
+      <table className="w-[90%] ">
         <thead>
-          <tr>
-            <th>Assigned to</th>
-            <th>Created at</th>
-            <th>Task</th>
-            <th>Status</th>
-            <th>Project</th>
+          <tr className="text-[#6d7174]">
+            <th className="font-light   text-left p-[1%]">Assigned to</th>
+            <th className="font-light  text-left p-[1%]">Created at</th>
+            <th className="font-light  text-left p-[1%]">Task</th>
+            <th className="font-light  text-right p-[1%]">Status</th>
+            <th className="font-light  text-left p-[1%]">Project</th>
           </tr>
         </thead>
         <tbody>
           {data.issues.map((el) =>
             el.assignees.length !== 0 ? ( //this line renders only the tasks that are assigned to someone
               <tr key={el.id}>
-                <td>
+                <td className=" text-left ">
                   {el.assignees.map((assignee) => (
-                    <span>
+                    <span className="flex flex-row items-center ">
                       <img
                         src={assignee.avatar_url}
-                        width={80}
-                        height={80}
+                        width={60}
+                        height={60}
                         alt="Picture of the contributor"
+                        className="rounded-[50%] p-[1%] m-[10%] bg-[white]"
                       />
-                      {assignee.login}
+                      <span>{assignee.login}</span>
                     </span>
                   ))}
                 </td>
-                <td>{el.created_at.slice(0, 10)}</td>
-                <td>{el.title}</td>
-                <td>{el.state}</td>
-                <td>{data.name}</td>
-                <td>
+
+                <td className="text-left p-[1%]">
+                  {el.created_at.slice(0, 10)}
+                </td>
+                <td className="text-left p-[1%]">{el.title}</td>
+                <td className="text-right p-[1%]">
+                  {el.state === "open" ? "In progress" : "Done"}
+                </td>
+                <td className="text-left p-[1%]">{data.name}</td>
+                <td className="text-left p-[1%]">
                   <a href={el.html_url}>See details</a>{" "}
                 </td>
               </tr>
