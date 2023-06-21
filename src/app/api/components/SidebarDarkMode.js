1494e52725ef14924f2bef0f83977c932377cb25
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-06-22 00:43:52 +0100

    Create SidebarDarkMode.js

---
+++ src/app/api/components/SidebarDarkMode.js
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle, faSun } from "@fortawesome/free-solid-svg-icons";
+const SidebarDarkMode = () => {
+  return (
+    <div className="flex flex-row gap-1 pt-16">
+      <div className="flex text-xs ">Light</div>
+      <div className="flex flex-row gap-1 bg-[#2A3131] rounded-sm ">
+        <span className="flex">
+          <FontAwesomeIcon icon={faSun} className="w-[12px] text-[#D9D9D9]" />
+        </span>
+        <span className="flex">
+          <FontAwesomeIcon
+            icon={faCircle}
+            className="w-[16px] text-[#37BCBA]"
+          />
+        </span>
+      </div>
+
+      <div className="flex text-xs">Dark</div>
+    </div>
+  );
+};
+export default SidebarDarkMode;
