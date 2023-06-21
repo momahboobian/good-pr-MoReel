7183d710867d90f0ff45daf299f789364a16872a
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-06-22 00:45:20 +0100

    Add Sidebar to page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
+import Sidebar from "./api/components/Sidebar";
 import TeamOverviewHeader from "./components/TeamOverviewHeader";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen justify-between p-6">
       <div className="flex border-neutral-800 rounded-3xl overflow-hidden">
-        {/* Content for the sidebar */}
+        <Sidebar />
         <div className="flex-1 w-screen bg-[#070E0E] p-6">
           <TeamOverviewHeader />
         </div>
