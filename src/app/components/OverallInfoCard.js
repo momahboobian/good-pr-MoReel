2c2381fd91e03614506f9747a346081e34073e0d
nataliizab <nataliia.zab@gmail.com>
2023-06-21 21:16:34 +0100

    connect to local  JSON file
    Currently, status of all the issues is open, so as a result we have 0 for both issues in progress and completed issues. This should work with the API, but I left the comments to double check once connected to the API.

--- src/app/components/OverallInfoCard.js
+++ src/app/components/OverallInfoCard.js
@@ -1,17 +1,24 @@
 import { FaShapes, FaExternalLinkAlt, FaSyncAlt } from "react-icons/fa";
-
+import data from "g1-e-commerce.json";
 
 export default function OverallInfoCard() {
   return (
-    <div className="grid grid-cols-1 gap-4 max-w-sm bg-[#111827] p-9 rounded-2xl">
+    <div className="grid grid-cols-1 gap-4 max-w-sm bg-[#1A1E1F] p-9 rounded-2xl">
       <div className="flex flex-column justify-between">
         <h2 className="text-[#F9F9F9] font-bold">Overall Information</h2>
         <button className="bg-[#111827] text-[#F9F9F9]">...</button>
       </div>
       <div className="flex flex-column justify-left gap-1 items-center my-4">
-        <div className="text-[#F9F9F9] font-bold text-2xl">67</div>
+        <div className="text-[#F9F9F9] font-bold text-2xl">
+          {" "}
+          {String(data.issues.filter((issue) => issue.state === "done").length)}
+          {/* when the issue.state is not open , it will calculate the number of completed issues. To double check with the real API, so we can know for sure how to track them       */}
+        </div>
         <div className="text-[#F9F9F9] ">|</div>
-        <div className="text-[#F9F9F9] font-bold text-2xl">105</div>
+        <div className="text-[#F9F9F9] font-bold text-2xl">
+          {String(data.issues[0].number)}
+          {/* number of issues in total. double-check accuracy of the code with the real API */}
+        </div>
         <p className="text-[#606467] text-xs ml-[10px] items-end">Tasks Done</p>
       </div>
       <div className="flex flex-column justify-between  mt-2">
@@ -20,7 +27,11 @@ export default function OverallInfoCard() {
             <div className="flex justify-center mt-2">
               <FaShapes />
             </div>
-            <p className="font-bold text-lg mt-2">28</p>
+            <p className="font-bold text-lg mt-2">
+              {" "}
+              {String(data.issues[0].number)}
+              {/* number of issues in total. To double-check accuracy of the code with the real API */}
+            </p>
             <p className="text-xs mt-2">Tasks</p>
           </div>
         </button>
@@ -29,7 +40,13 @@ export default function OverallInfoCard() {
             <div className="flex justify-center mt-2">
               <FaSyncAlt />
             </div>
-            <p className="font-bold text-lg mt-2">17</p>
+            <p className="font-bold text-lg mt-2">
+              {String(
+                data.issues.filter((issue) => issue.state == "In Progress")
+                  .length
+              )}
+              {/* number of issues in progress. To double-check accuracy of the status name in the real API */}
+            </p>
             <p className="text-xs mt-2">In progress</p>
           </div>
         </button>
@@ -38,7 +55,13 @@ export default function OverallInfoCard() {
             <div className="flex justify-center mt-2">
               <FaExternalLinkAlt />
             </div>
-            <p className="font-bold text-lg mt-2">11</p>
+            <p className="font-bold text-lg mt-2">
+              {" "}
+              {String(
+                data.issues.filter((issue) => issue.state == "done").length
+              )}
+              {/* number of completed issues. To double-check accuracy of the status name in the real API */}
+            </p>
             <p className="text-xs mt-2">Completed</p>
           </div>
         </button>
