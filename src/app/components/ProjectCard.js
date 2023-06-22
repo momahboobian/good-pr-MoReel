aac14148fc6528714cdb0b5aba973b478a43bd5b
nataliizab <nataliia.zab@gmail.com>
2023-06-22 17:04:18 +0100

    Create a TeamActivity component

--- src/app/components/ProjectCard.js
+++ src/app/components/ProjectCard.js
@@ -1,9 +1,9 @@
 const ProjectCard = () => {
   return (
-    <div className="flex flex-col gap-6 pt-10">
+    <div className="flex flex-col gap-6 pt-10 ">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-4">
-          <div className="flex flex-col justify-between h-[250px] bg-[#1A1E1F] rounded-xl p-4">
+          <div className="flex flex-col justify-between h-[250px] bg-[#1A1E1F] rounded-2xl p-4 ">
             <h1 className="font-bold text-sm text-white">Project</h1>
             <button className="bg-[#37BCBA] text-black rounded-lg p-2 pl-6 pr-6">
               See All Project
---
+++ src/app/components/TeamActivity.js
@@ -0,0 +1,14 @@
+import data from "g1-e-commerce.json";
+
+export default function TeamActivity() {
+  return (
+    <div className="grid grid-cols-1 gap-4 max-w-sm bg-[#1A1E1F] p-9 rounded-2xl">
+      <div className="flex flex-column justify-between">
+        <h2 className="text-[#F9F9F9] font-bold">Team Activity</h2>
+      
+      </div>
+    
+    
+    </div>
+  );
+}
