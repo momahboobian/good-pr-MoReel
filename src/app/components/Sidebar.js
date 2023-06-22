198b9f00f260758022fbc0ef54ea6c4b93747047
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-06-22 16:52:50 +0100

    Update width Sidebar

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -5,7 +5,7 @@ import SidebarDarkMode from "./SidebarDarkMode";
 
 const Sidebar = () => {
   return (
-    <div className="flex flex-col items-center gap-10 pt-10 p-10 w-[200px] bg-[#1A1E1F] text-white">
+    <div className="flex flex-col items-center gap-10 pt-10 p-10 w-96  bg-[#1A1E1F] text-white">
       <SidebarLogo />
       <SidebarDashboard />
       <SidebarTeams />
