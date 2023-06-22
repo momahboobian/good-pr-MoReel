e74bd53daadb108eeb7cc48e34f59e7ccffa4a11
nataliizab <nataliia.zab@gmail.com>
2023-06-22 09:00:46 +0100

    Update OverallInfoCard.js

--- src/app/components/OverallInfoCard.js
+++ src/app/components/OverallInfoCard.js
@@ -6,7 +6,7 @@ export default function OverallInfoCard() {
     <div className="grid grid-cols-1 gap-4 max-w-sm bg-[#1A1E1F] p-9 rounded-2xl">
       <div className="flex flex-column justify-between">
         <h2 className="text-[#F9F9F9] font-bold">Overall Information</h2>
-        <button className="bg-[#111827] text-[#F9F9F9]">...</button>
+        <button className=" text-[#F9F9F9]">...</button>
       </div>
       <div className="flex flex-column justify-left gap-1 items-center my-4">
         <div className="text-[#F9F9F9] font-bold text-2xl">
@@ -22,7 +22,7 @@ export default function OverallInfoCard() {
         <p className="text-[#606467] text-xs ml-[10px] items-end">Tasks Done</p>
       </div>
       <div className="flex flex-column justify-between  mt-2">
-        <button className="w-20 bg-[#E2E949] rounded-xl p-3">
+        <button className="w-20 bg-[#E2E949] rounded-xl p-3 flex justify-center items-center">
           <div className="w-16 ">
             <div className="flex justify-center mt-2">
               <FaShapes />
@@ -32,10 +32,10 @@ export default function OverallInfoCard() {
               {String(data.issues[0].number)}
               {/* number of issues in total. To double-check accuracy of the code with the real API */}
             </p>
-            <p className="text-xs mt-2">Tasks</p>
+            <p className="text-xs mt-2 text-black">Tasks</p>
           </div>
         </button>
-        <button className="w-20 bg-[#36BCBA] rounded-xl p-3">
+        <button className="w-20 bg-[#36BCBA] rounded-xl p-3 flex flex-col justify-center items-center">
           <div className="w-16">
             <div className="flex justify-center mt-2">
               <FaSyncAlt />
@@ -47,10 +47,10 @@ export default function OverallInfoCard() {
               )}
               {/* number of issues in progress. To double-check accuracy of the status name in the real API */}
             </p>
-            <p className="text-xs mt-2">In progress</p>
+            <p className="text-xs mt-2 text-black">In progress</p>
           </div>
         </button>
-        <button className="w-20 bg-[#F55706] rounded-xl p-3">
+        <button className="w-20 bg-[#F55706] rounded-xl p-3 flex flex-col justify-center items-center">
           <div className="w-16">
             <div className="flex justify-center mt-2">
               <FaExternalLinkAlt />
@@ -58,11 +58,12 @@ export default function OverallInfoCard() {
             <p className="font-bold text-lg mt-2">
               {" "}
               {String(
-                data.issues.filter((issue) => issue.state == "done").length
+                data.issues.filter((issue) => issue.state === "completed")
+                  .length
               )}
               {/* number of completed issues. To double-check accuracy of the status name in the real API */}
             </p>
-            <p className="text-xs mt-2">Completed</p>
+            <p className="text-xs mt-2 text-black">Completed</p>
           </div>
         </button>
       </div>
