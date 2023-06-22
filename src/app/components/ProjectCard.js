fae359054fbd8c8f4285ebcad411086cc3dc6212
Mo Mahboobian <momahboobian@gmail.com>
2023-06-22 23:22:54 +0100

    added date and fine tuning the style

--- src/app/components/ProjectCard.js
+++ src/app/components/ProjectCard.js
@@ -1,49 +1,72 @@
+import { FaCalendarAlt } from "react-icons/fa";
 import data from "g1-e-commerce.json";
-import Image from "next/image";
 
-const avatarBoarder = (state) => {
-  if (state === "open") {
-    return "border-yellow-200";
-  } else if (state === "closed") {
-    return "border-orange-600";
-  } else if (state === "close") {
-    return "border-cyan-700";
-  } else if (state === "d") {
-    return "border-violet-600	";
-  }
+const avatarBorderColor = (index) => {
+  const colors = [
+    "border-yellow-200",
+    "border-orange-600",
+    "border-cyan-700",
+    "border-violet-600",
+    "border-yellow-700	",
+  ];
+  const colorIndex = index % colors.length;
+  return colors[colorIndex];
 };
 
 const ProjectCard = () => {
+  const currentDate = new Date();
+  const options = { day: "numeric", month: "long", year: "numeric" };
+  const formattedDate = currentDate.toLocaleDateString(undefined, options);
+
   return (
     <div className="flex flex-col gap-6 pt-10 pr-10">
-      {/* <div className="flex items-center justify-between">*/}
       <div className="flex items-center gap-4">
-        <div className="flex flex-col justify-between h-80 w-72 bg-[#1A1E1F] rounded-2xl p-9 ">
-          <h1 className="font-bold text-s text-white">Project</h1>
-          <div className="border-2 border-[#37BCBA] rounded-lg">
-            <div className="relative">
-              {data.assignees.map((assignee, index) => (
-                <img
-                  key={assignee.id}
-                  src={assignee.avatar_url}
-                  alt={assignee.login}
-                  className={`w-11 h-11 rounded-full border-2 object-cover absolute transform translate-x-1/2 translate-y-1/2`}
-                  style={{
-                    zIndex: index + 1,
-                    marginLeft: `${index * 2}rem`,
-                  }}
-                />
-              ))}
-            </div>
+        <div className="flex flex-col justify-between h-80 w-80 bg-[#1A1E1F] rounded-2xl p-9 ">
+          <div className="flex justify-between items-center">
+            <h1 className="font-bold text-s text-white">Project</h1>
+            <a href="#" className="text-gray-500 text-lg ">
+              ...
+            </a>
           </div>
-
-          <button className="bg-[#37BCBA] text-black rounded-lg p-2 pl-6 pr-6">
+          <div className="flex items-center justify-between mt-4">
+            <span className="w-12 h-2 bg-yellow-300 rounded-full mx-1"></span>
+            <span className="w-12 h-2 bg-yellow-300 rounded-full mx-1"></span>
+            <span className="w-12 h-2 relative mx-1">
+              <span className="absolute left-0 top-0 w-1/2 h-full bg-yellow-300 rounded-l-full "></span>
+              <span className="absolute right-0 top-0 w-1/2 h-full bg-gray-500 rounded-r-full "></span>
+            </span>
+            <span className="w-12 h-2 bg-gray-500 rounded-full mx-1"></span>
+          </div>{" "}
+          <div className="flex just border rounded-full border-gray-600 p-1">
+            {data.assignees.map((assignee, index) => (
+              <img
+                key={assignee.id}
+                src={assignee.avatar_url}
+                alt={assignee.login}
+                className={`w-11 h-11 rounded-full border-2 object-cover
+                 ${avatarBorderColor(index)}
+                }`}
+                style={{
+                  zIndex: index + 1,
+                  position: "relative",
+                  left: `${index * -6}%`,
+                }}
+              />
+            ))}
+            <p className="flex items-center text-white text-sm font-bold relative right-2 ">
+              +4
+            </p>
+          </div>
+          <div className="flex items-left items-center text-gray-500 text-xs text-center">
+            <FaCalendarAlt className="mr-1 text-[#37BCBA] text-lg" />
+            {formattedDate}
+          </div>{" "}
+          <button className="bg-[#37BCBA] text-black rounded-lg p-2 font-semibold">
             See All Project
           </button>
         </div>
       </div>
     </div>
-    // </div>
   );
 };
 
