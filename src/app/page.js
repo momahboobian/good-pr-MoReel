1618c0a207dc0d447d1d82e42e0054f145d27194
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-06-22 13:17:44 +0100

    Move Sidebar to the app component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import Sidebar from "./api/components/Sidebar";
+import Sidebar from "./components/Sidebar";
 import TeamOverviewHeader from "./components/TeamOverviewHeader";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen justify-between">
       <div className="flex border-neutral-800  overflow-hidden">
-        <Sidebar />        
+        <Sidebar />
 
         <div className="flex-1 w-screen bg-[#070E0E] p-6">
           <TeamOverviewHeader />
