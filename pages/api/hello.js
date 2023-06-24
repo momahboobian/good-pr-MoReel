493059e3310a30821a6be2c37290576d64055c80
Lorena <lorenacapraru@gmail.com>
2023-06-24 18:30:52 +0100

    API create

---
+++ pages/api/hello.js
@@ -0,0 +1,44 @@
+const { Octokit } = require("@octokit/rest");
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req, res) => {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_AUTH_TOKEN,
+  });
+
+  const repo = await octokit.request(
+    "https://api.github.com/repos/nataliiazab/good-pr"
+  );
+  const issues = await octokit.request(
+    "https://api.github.com/repos/nataliiazab/good-pr/issues"
+  );
+  const contributors = await octokit.request(
+    "https://api.github.com/repos/nataliiazab/good-pr/contributors"
+  );
+  const languages = await octokit.request(
+    "https://api.github.com/repos/nataliiazab/good-pr/languages"
+  );
+
+  const contributorsNames = contributors.data.map((el) => el.login);
+  console.log(repo.data.owner.login);
+  console.log(repo.data.name);
+  const owner = repo.data.owner.login;
+  const repos = repo.data.name;
+
+  const pr = await Promise.all(
+    contributorsNames.map(
+      async (contributorName) =>
+        await octokit.request("GET /search/issues", {
+          q: `is:pr repo:${owner}/${repos} author:${contributorName}`,
+        })
+    )
+  );
+
+  return res.status(200).json({
+    repo: repo,
+    issues: issues,
+    contributors: contributors,
+    languages: languages,
+    pr: pr,
+  });
+};
