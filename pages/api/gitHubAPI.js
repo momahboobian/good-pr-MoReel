fc2bafffe6fe0eae7348e4bf251724aa712aac7f
Lorena <lorenacapraru@gmail.com>
2023-06-24 18:56:33 +0100

    Rendered PRs based on assignees instead contributors

--- pages/api/gitHubAPI.js
+++ pages/api/gitHubAPI.js
@@ -7,34 +7,38 @@ export default async (req, res) => {
   });
 
   const owner = "nataliiazab";
-  const repos = "good-pr";
+  const repository = "good-pr";
 
+  const assignees = await octokit.request(
+    `https://api.github.com/repos/${owner}/${repository}/assignees`
+  );
   const repo = await octokit.request(
-    "https://api.github.com/repos/nataliiazab/good-pr"
+    `https://api.github.com/repos/${owner}/${repository}`
   );
   const issues = await octokit.request(
-    "https://api.github.com/repos/nataliiazab/good-pr/issues"
+    `https://api.github.com/repos/${owner}/${repository}/issues`
   );
   const contributors = await octokit.request(
-    "https://api.github.com/repos/nataliiazab/good-pr/contributors"
+    `https://api.github.com/repos/${owner}/${repository}/contributors`
   );
   const languages = await octokit.request(
-    "https://api.github.com/repos/nataliiazab/good-pr/languages"
+    `https://api.github.com/repos/${owner}/${repository}/languages`
   );
 
-  const contributorsNames = contributors.data.map((el) => el.login); //used to retrieve individual PRs
+  const contributorsNames = assignees.data.map((el) => el.login); //used to retrieve individual PRs
 
   const pr = await Promise.all(
     contributorsNames.map(
       async (contributorName) =>
         await octokit.request("GET /search/issues", {
-          q: `is:pr repo:${owner}/${repos} author:${contributorName}`,
+          q: `is:pr repo:${owner}/${repository} author:${contributorName}`,
         })
     )
   );
 
   return res.status(200).json({
     repo: repo,
+    assignees: assignees,
     issues: issues,
     contributors: contributors,
     languages: languages,
