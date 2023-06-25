51f5fd2882b204f1d01d54f0e5053aee0ae23e7f
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-06-25 20:07:50 +0100

    Update TeamOverviewHeader.js

--- src/app/components/TeamOverviewHeader.js
+++ src/app/components/TeamOverviewHeader.js
@@ -5,17 +5,18 @@ import TeamActivity from "./TeamActivity";
 
 const TeamOverview = () => {
   return (
-    <div class="p-6">
+    <div class=" p-6">
       {/* Content for the right div */}
-      <h1 className="font-bold text-white px-6 ">Team Overview</h1>
-      <p className="font-light	text-xs	text-gray-600	pt-1 px-6 ">
+      <h1 className="font-bold text-white p-6 ">Team Overview</h1>
+      <p className="font-light	text-xs	text-gray-600	pt-1 p-6 ">
         Track you projects, tasks & team activity here
       </p>
-      <div className="flex flex-row mt-4 w-full space-x-4 gap-4 p-6">
+      <div className=" flex flex-row justify-self-center mt-4 gap-20 p-6 ">
         <ProjectCard />
         <OverallInfoCard />
         <TeamActivity />
       </div>
+
       <TaskActivity />
     </div>
   );
