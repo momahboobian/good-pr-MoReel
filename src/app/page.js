0c39b3d42de5388b6e4286d8a1e9631121647dad
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-06-25 14:45:47 +0100

    Add padding to the main

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import TeamOverviewHeader from "./components/TeamOverviewHeader";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-between">
+    <main className="flex min-h-screen justify-between p-0">
       <div className="flex border-neutral-800  overflow-hidden rounded-lg">
         {/* Content for the sidebar */}
         <Sidebar />
