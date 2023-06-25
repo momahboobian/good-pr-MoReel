08a065771d5db0711084c7eb0e63d9ae13713024
Mo Mahboobian <momahboobian@gmail.com>
2023-06-26 00:54:24 +0100

    adding babel-eslint eslint-config-next libraries and modifying the tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
       },
     },
   },
-  content: ["./**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
