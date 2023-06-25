8fe9e889d84ac9a73d1de5fea83cdff74948c141
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-06-25 21:48:40 +0100

    Align the width of teamOverview and taskActivity

--- src/app/components/TeamOverviewHeader.js
+++ src/app/components/TeamOverviewHeader.js
@@ -7,11 +7,11 @@ const TeamOverview = () => {
   return (
     <div class=" p-6">
       {/* Content for the right div */}
-      <h1 className="font-bold text-white p-6 ">Team Overview</h1>
-      <p className="font-light	text-xs	text-gray-600	pt-1 p-6 ">
+      <h1 className="font-bold text-white p-4 ">Team Overview</h1>
+      <p className="font-light	text-xs	text-gray-600	pt-1 p-4 ">
         Track you projects, tasks & team activity here
       </p>
-      <div className=" flex flex-row justify-self-center mt-4 gap-20 p-6 ">
+      <div className=" flex flex-row justify-between mt-4 gap-4 p-6 ">
         <ProjectCard />
         <OverallInfoCard />
         <TeamActivity />
