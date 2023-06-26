1969de3e61fafda172441464192b2f098385310a
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2023-06-26 21:38:29 +0100

    created the frontend of the Teams pages

--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
+import Sidebar from "@components/Sidebar";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen justify-between p-0">
-      <div className="">This is from page</div>
+      <div className="flex border-neutral-800  overflow-hidden rounded-lg">
+        <Sidebar />
+        <div className="flex-1 w-screen bg-[#F1F3F7] p-6">
+          <div>
+            <h1 className="text-2xl font-bold">Teams</h1>
+            <p className="text-sm ">Track teams & members</p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
