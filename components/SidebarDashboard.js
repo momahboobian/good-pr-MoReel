0b8c72c90670bdf974f9df16c97e8123fc83efd8
Mo Mahboobian <momahboobian@gmail.com>
2023-06-30 00:47:15 +0100

    Fixing navlink active colour

--- components/SidebarDashboard.js
+++ components/SidebarDashboard.js
@@ -15,11 +15,7 @@ export default function SidebarDashboard() {
     <ul className="flex flex-col items-start gap-6 pt-14">
       <li className="flex items-center text-sm">
         <Link href="/">
-          <span
-            className={`${
-              pathname === "/" ? "text-cyan-600" : "text-[#37BCBA]"
-            }`}
-          >
+          <span className={`${pathname === "/" ? "text-[#37BCBA]" : ""}`}>
             <FontAwesomeIcon icon={faHouseUser} className="w-[15px] mr-3" />
             Home
           </span>
@@ -28,7 +24,7 @@ export default function SidebarDashboard() {
       <li className="flex items-center text-sm">
         <Link href="/dashboard">
           <span
-            className={`${pathname === "/dashboard" ? "text-cyan-600" : ""}`}
+            className={`${pathname === "/dashboard" ? "text-[#37BCBA]" : ""}`}
           >
             <FontAwesomeIcon icon={faColumns} className="w-[15px] mr-3" />
             Dashboard
@@ -38,7 +34,7 @@ export default function SidebarDashboard() {
       <li className="flex items-center text-sm">
         <Link href="/my-tasks">
           <span
-            className={`${pathname === "/my-tasks" ? "text-cyan-600" : ""}`}
+            className={`${pathname === "/my-tasks" ? "text-[#37BCBA]" : ""}`}
           >
             <FontAwesomeIcon icon={faListCheck} className="w-[15px] mr-3" />
             My Tasks
@@ -48,7 +44,7 @@ export default function SidebarDashboard() {
       <li className="flex items-center text-sm">
         <Link href="/statistics">
           <span
-            className={`${pathname === "/statistics" ? "text-cyan-600" : ""}`}
+            className={`${pathname === "/statistics" ? "text-[#37BCBA]" : ""}`}
           >
             <FontAwesomeIcon icon={faChartColumn} className="w-[15px] mr-3" />
             Statistics
