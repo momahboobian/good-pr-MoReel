9d1231e4299da032d7ee3f60b517d7bd0c5e0d05
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-06-29 10:19:40 +0100

    Make the logo clickable

--- components/SidebarLogo.js
+++ components/SidebarLogo.js
@@ -1,18 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-
+import Link from "next/link";
 export default function SidebarLogo() {
   return (
-    <div className="flex items-center">
-      <FontAwesomeIcon
-        icon={faGithub}
-        className="text-[#37BCBA] w-[20px] mr-1"
-      />
+    <Link href="/" className="flex items-center">
+        <FontAwesomeIcon
+          icon={faGithub}
+          className="text-[#37BCBA] w-[20px] mr-1"
+        />
 
-      <h1 className="font-bold text-xl  text-white">
-        <span className="mr-1">GOOD</span>
-        <span className="text-[#37BCBA]">PR</span>
-      </h1>
-    </div>
+        <h1 className="font-bold text-xl  text-white">
+          <span className="mr-1">GOOD</span>
+          <span className="text-[#37BCBA]">PR</span>
+        </h1>
+    </Link>
   );
 }
