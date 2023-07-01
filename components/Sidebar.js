04b387a46a187acf3b0b029a45fe850fe7d3123d
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-07-01 13:36:27 +0100

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -39,7 +39,7 @@ export default function Sidebar() {
           <FiMenu size={48} /> {/* Increase the size to 48 */}
         </div>
         <div
-          className={`fixed top-0 left-0 w-56 h-full bg-black bg-opacity-50 transform transition-transform duration-300 ${
+          className={`fixed z-20 first-letter:top-0 left-0 w-56 h-full bg-black bg-opacity-50 transform transition-transform duration-300 ${
             showSidebar ? "translate-x-0 w-1/3" : "-translate-x-full"
           }`}
         >
