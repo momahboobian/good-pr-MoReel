fa8e339a1ff3b3040ff35639fe041a9d55f4ed91
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-07-02 23:16:45 +0100

    fixed a loading bug

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -15,8 +15,9 @@ const avatarBorderColor = (index) => {
 
 // Calculate the start and end dates for the progress line
 const calculateProgressDates = (repo) => {
-  const startDate = new Date(repo.created_at);
-  const endDate = new Date(startDate.getTime() + 4 * 7 * 24 * 60 * 60 * 1000); // 4 weeks in milliseconds
+  const startDate = repo && new Date(repo.created_at);
+  const endDate =
+    startDate && new Date(startDate.getTime() + 4 * 7 * 24 * 60 * 60 * 1000); // 4 weeks in milliseconds
   return { startDate, endDate };
 };
 
@@ -41,7 +42,7 @@ export default function ProjectCard({ repo, pr }) {
     }
   };
 
-  const trainees = pr.filter((el) => el.total_count !== 0);
+  const trainees = pr && pr.filter((el) => el.total_count !== 0);
 
   const { startDate, endDate } = calculateProgressDates(repo);
 
@@ -49,61 +50,59 @@ export default function ProjectCard({ repo, pr }) {
   const totalDays = Math.round((endDate - startDate) / (1000 * 60 * 60 * 24));
 
   return (
-    <div className="flex flex-cols-1 gap-4 max-w-sm bg-[#1A1E1F] rounded-2xl">
-      <div className="flex items-start gap-4">
-        <div className="flex flex-col justify-between h-80 w-[22rem] bg-[#1A1E1F] rounded-2xl p-6 relative">
-          <div className="flex justify-between items-center">
-            <h1 className="font-bold text-s text-white">Project</h1>
-          </div>
-          <div className="flex justify-between items-center mt-4">
-            {/* <div className="bg-gray-300 h-2 flex-1 mr-2 rounded-full"></div> */}
-            <div
-              className="bg-yellow-500 h-2 rounded-full relative"
-              style={{
-                width: `${
-                  ((Date.now() - startDate) / (endDate - startDate)) * 100
-                }%`,
-              }}
-            >
-              <div className="absolute top-[-18px] text-[#606467] text-xs rounded-full">
-                {totalDays} days
-              </div>
-              <div className="absolute top-0 right-0 w-2 h-2 bg-yellow-500 transform rotate-45 translate-x-1/2"></div>
+    <div className="flex flex-col max-w-sm md:max-w-lg xl:max-w-xl bg-[#1A1E1F] rounded-2xl">
+      <div className="flex flex-col justify-between h-80 w-[22rem] bg-[#1A1E1F] rounded-2xl p-6 relative">
+        <div className="flex justify-between items-center">
+          <h1 className="font-bold text-s text-white">Project</h1>
+        </div>
+        <div className="flex justify-between items-center mt-4">
+          {/* <div className="bg-gray-300 h-2 flex-1 mr-2 rounded-full"></div> */}
+          <div
+            className="bg-yellow-500 h-2 rounded-full relative"
+            style={{
+              width: `${
+                ((Date.now() - startDate) / (endDate - startDate)) * 100
+              }%`,
+            }}
+          >
+            <div className="absolute top-[-18px] text-[#606467] text-xs rounded-full">
+              {totalDays} days
             </div>
-            <div className="bg-gray-300 h-2 flex-1 ml-3 rounded-full"></div>
-            <p className="text-[#606467] text-xs items-end pl-3 whitespace-nowrap">
-              Team Progress
-            </p>
+            <div className="absolute top-0 right-0 w-2 h-2 bg-yellow-500 transform rotate-45 translate-x-1/2"></div>
           </div>
+          <div className="bg-gray-300 h-2 flex-1 ml-3 rounded-full"></div>
+          <p className="text-[#606467] text-xs items-end pl-3 whitespace-nowrap">
+            Team Progress
+          </p>
+        </div>
 
-          <div className="flex just border rounded-full border-gray-600 p-1">
-            {trainees.map((trainee, index) => (
-              <Image
-                key={trainee.items[0].user.id}
-                src={trainee.items[0].user.avatar_url}
-                width={40}
-                height={40}
-                alt={trainee.items[0].user.login}
-                className={`w-14 h-14 rounded-full border-2 object-cover
+        <div className="flex just border rounded-full border-gray-600 p-1">
+          {trainees.map((trainee, index) => (
+            <Image
+              key={trainee.items[0].user.id}
+              src={trainee.items[0].user.avatar_url}
+              width={40}
+              height={40}
+              alt={trainee.items[0].user.login}
+              className={`w-14 h-14 rounded-full border-2 object-cover
                  ${avatarBorderColor(index)}
                 }`}
-                style={{
-                  zIndex: index + 1,
-                  position: "relative",
-                  left: `${index * -5}%`,
-                }}
-              />
-            ))}
-          </div>
+              style={{
+                zIndex: index + 1,
+                position: "relative",
+                left: `${index * -5}%`,
+              }}
+            />
+          ))}
+        </div>
 
-          <div className="flex justify-left items-center pb-1">
-            <div className="text-[#F9F9F9] font-bold text-2xl">
-              {lastActivityDate()}
-            </div>
-            <p className="text-[#606467] text-xs pl-4 items-end ">
-              Last Activity
-            </p>
+        <div className="flex justify-left items-center pb-1">
+          <div className="text-[#F9F9F9] font-bold text-2xl">
+            {lastActivityDate()}
           </div>
+          <p className="text-[#606467] text-xs pl-4 items-end ">
+            Last Activity
+          </p>
         </div>
       </div>
     </div>
