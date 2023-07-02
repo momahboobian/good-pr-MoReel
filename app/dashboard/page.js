3f29ac0ddb0c9b4b70f3959ddbb33369072eb2db
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-07-02 23:15:35 +0100

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,7 +3,7 @@ import TeamOverviewHeader from "@components/TeamOverviewHeader";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-between md:p-5">
+    <main className="flex justify-center min-h-screen md:p-5">
       <div className="flex border-neutral-800 overflow-hidden rounded-lg">
         <Sidebar />
         <div className="flex-1 w-screen bg-[#070E0E]">
