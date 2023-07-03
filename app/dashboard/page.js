a30369f2199f43e081e845155af15a2a4233f6b8
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-07-03 10:43:44 +0100

    align the cards on the dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,9 +3,11 @@ import TeamOverviewHeader from "@components/TeamOverviewHeader";
 
 export default function Home() {
   return (
-    <main className="flex justify-center min-h-screen md:p-5">
-      <div className="flex border-neutral-800 overflow-hidden rounded-lg">
-        <Sidebar />
+  <main className="flex justify-center items-center min-h-screen md:p-5 ">
+      <div className="flex border-neutral-800 overflow-hidden rounded-lg md:w-[1380px]">
+        <div>
+          <Sidebar />
+        </div>
         <div className="flex-1 w-screen bg-[#070E0E]">
           <TeamOverviewHeader />
         </div>
--- components/TeamOverviewHeader.js
+++ components/TeamOverviewHeader.js
@@ -54,7 +54,7 @@ export default function TeamOverview() {
         <p className="font-light text-xs text-gray-500 pt-1 p-4">
           Track you projects, tasks & team activity here
         </p>
-        <div className="grid sm:flex space-y-6 mt-4 gap-4 p-3 md:p-6">
+        <div className="grid sm:flex md:space-y-0 mt-4 gap-4 p-3 md:p-6">
           <ProjectCard repo={repo} pr={pr} />
           <TeamActivityPie pr={pr} />
           <OverallInfoCard
