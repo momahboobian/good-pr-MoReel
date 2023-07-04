c9e3c0460f0625d985a6a121bcc746a640f2192a
Natalie Zablotska <113237479+nataliiazab@users.noreply.github.com>
2023-07-04 20:32:07 +0100

    Revert "Natalie link the team page with the database #84"

--- components/GroupsPage.js
+++ components/GroupsPage.js
@@ -1,30 +1,18 @@
-"use client";
-import React, { useEffect, useState } from "react";
-// import groupsData from "groups.json";
+import React from "react";
+import groupsData from "groups.json";
 import TeamCard from "./TeamCard";
 
 export default function GroupsPage() {
-  const [groups, setGroups] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/repositories");
-        const data = await response.json();
-        setGroups(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const getRandomAvatar = (collaborators) => {
+    const randomIndex = Math.floor(Math.random() * collaborators.length);
+    return collaborators[randomIndex].avatar_url;
+  };
 
   return (
     <div className="flex flex-row flex-wrap gap-4   w-full items-center justify-around">
-      {groups.map((group) => (
+      {groupsData.groups.map((group) => (
         <div key={group.id}>
-          <TeamCard group={group} />
+          <TeamCard group={group} getRandomAvatar={getRandomAvatar} />
         </div>
       ))}
     </div>
