63471b14be5c07cfe00f0f7dd220690b82c7169d
Mo Mahboobian <momahboobian@gmail.com>
2023-07-06 11:16:07 +0100

    Adding favicon

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "@styles/globals.css";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,8 +12,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </>
   );
 }
