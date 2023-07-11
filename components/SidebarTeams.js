5ce265392ab00cbe14363ad83ed09b70155cdcd4
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-07-11 13:23:01 +0100

    Groups linked in the dashboard sidebar

--- components/SidebarTeams.js
+++ components/SidebarTeams.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faSquareArrowUpRight,
@@ -5,75 +7,51 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 export default function SidebarTeams() {
+  const [groups, setGroups] = useState([]);
   const pathname = usePathname();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/repositories");
+        const data = await response.json();
+        setGroups(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <ul className="flex flex-col items-start gap-6 pt-40">
       <li className="flex items-center ml-3 text-sm text-[#717578]active:text-cyan-600">
-        <Link href="#">
-          <span
-            className={`${
-              pathname === "#" ? "text-cyan-600" : "text-[#717578]"
-            }`}
-          >
-            TEAMS
-          </span>
-        </Link>
+        <span
+          className={`${pathname === "#" ? "text-cyan-600" : "text-[#717578]"}`}
+        >
+          TEAMS
+        </span>
       </li>
-      <li className="flex items-center text-xs active:text-cyan-600">
-        <Link href="#">
-          <span
-            className={`${pathname === "#" ? "text-cyan-600" : "text-white"}`}
-          >
-            <FontAwesomeIcon
-              icon={faSquareArrowUpRight}
-              className="w-[15px] mr-3 text-[#8F46EA]"
-            />
-            Group 2
-          </span>
+      {groups.map((group) => (
+        <Link key={group.id} href={`/dashboard?id=${group.id}`}>
+          <li className="flex items-center text-xs active:text-cyan-600">
+            <span
+              className={`${pathname === "#" ? "text-cyan-600" : "text-white"}`}
+            >
+              <FontAwesomeIcon
+                icon={faSquareArrowUpRight}
+                className="w-[15px] mr-3 text-[#8F46EA]"
+              />
+              {group.name}
+            </span>
+          </li>
         </Link>
-      </li>
-      <li className="flex items-center text-xs active:text-cyan-600">
-        <Link href="#">
-          <span
-            className={`${pathname === "#" ? "text-cyan-600" : "text-white"}`}
-          >
-            <FontAwesomeIcon
-              icon={faSquareArrowUpRight}
-              className="w-[15px] mr-3 text-[#E3820C]"
-            />
-            Group 3
-          </span>
-        </Link>
-      </li>
-      <li className="flex items-center text-xs active:text-cyan-600">
-        <Link href="#">
-          <span
-            className={`${pathname === "#" ? "text-cyan-600" : "text-white"}`}
-          >
-            <FontAwesomeIcon
-              icon={faSquareArrowUpRight}
-              className="w-[15px] mr-3 text-[#E3EA46]"
-            />
-            Group 4
-          </span>
-        </Link>
-      </li>
-      <li className="flex items-center text-xs active:text-cyan-600">
-        <Link href="#">
-          <span
-            className={`${pathname === "#" ? "text-cyan-600" : "text-white"}`}
-          >
-            <FontAwesomeIcon
-              icon={faSquarePlus}
-              className="w-[15px] mr-3 text-[#D9D9D9]"
-            />
-            View all
-          </span>
-        </Link>
-      </li>
+      ))}
     </ul>
   );
 }
