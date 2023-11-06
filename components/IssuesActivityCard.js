238e8107597946dbb75a9db7932ac903112df74b
Lorena <lorenacapraru@gmail.com>
2023-11-06 11:33:36 +0000

    clean code by deleting console.logs

--- components/IssuesActivityCard.js
+++ components/IssuesActivityCard.js
@@ -38,15 +38,10 @@ export default function IssuesActivityCard({ issuesClosed, pr }) {
   //to have the same trainees as in the project Card
   const trainees = (pr && pr.filter((el) => el.total_count !== 0)) || [];
   const issuesAssignee = trainees.map((trainee) => trainee.items[0].user.login);
-  console.log(
-    "tr",
-    trainees.map((trainee) => trainee.items[0].user.login)
-  );
 
   useEffect(() => {
     const chartData = names.map((user, index) => {
       const total_count = user.length || 0;
-      console.log(total_count);
       return {
         name: filterAndTruncateName(
           user[0] ? user[0].assignees[0].login : issuesAssignee[index],
