c7673be9953eb5d005f5b423d5b8545670cd1430
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2023-12-06 22:14:06 +0000

    delete my tasks and statistics - they file we haven't developed and are not going to in the near future

--- app/dashboard/[id]/page.js
+++ app/dashboard/[id]/page.js
@@ -1,11 +1,11 @@
 "use client";
 
-import TeamOverviewHeader from "@components/Dashboard/Dashboard";
+import Dashboard from "@components/Dashboard/Dashboard";
 
 export default function dashboardId() {
   return (
     <div className="flex justify-center items-start bg-[#070E0E] h-full md:w-screen">
-      <TeamOverviewHeader />
+      <Dashboard />
     </div>
   );
 }
