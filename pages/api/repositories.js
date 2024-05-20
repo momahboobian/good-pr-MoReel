736c657fdea95b39496e7217a3ec9e4acecd88e0
Mo Mahboobian <momahboobian@gmail.com>
2024-05-20 20:34:38 +0100

    Log the detailed error message without exposing sensitive information

--- pages/api/repositories.js
+++ pages/api/repositories.js
@@ -6,12 +6,9 @@ export default async function handler(req, res) {
     res.status(200).json(repos);
   } catch (error) {
     console.error("Error fetching repositories:", error);
-    res
-      .status(500)
-      .json({
-        message: "An unexpected error occurred while fetching repositories.",
-        error: error.message,
-      });
+    res.status(500).json({
+      message: "An unexpected error occurred while fetching repositories.",
+    });
   } finally {
     await prisma.$disconnect();
   }
