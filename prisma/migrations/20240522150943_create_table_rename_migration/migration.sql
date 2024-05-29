3a17e4469261131da6ed58f55750523960aa09f9
Mo Mahboobian <momahboobian@gmail.com>
2024-05-29 13:27:43 +0100

    Last modifcation and Updating migration file

--- prisma/migrations/20240522150943_create_table_rename_migration/migration.sql
+++ prisma/migrations/20240522150943_create_table_rename_migration/migration.sql
@@ -8,24 +8,21 @@
   - Changed the column `github_url` on the `Repository` table to `repo_url`.
   - A unique constraint covering the columns `[repo_name]` on the table `Repository` will be added. If there are existing duplicate values, this will fail.
 
-  
 */
-
-
 -- Create a backup
 CREATE TABLE "Repository_backup" AS TABLE "Repository";
 
--- AlterTable adding new columns
+-- AlterTable
 ALTER TABLE "Repository" DROP CONSTRAINT "Repository_pkey",
-DROP COLUMN "id",
-ADD COLUMN  "id" INTEGER NOT NULL,
 ADD COLUMN  "github_url" TEXT,
 ADD COLUMN  "demo_url" TEXT,
 ADD COLUMN  "cohort" TEXT,
 ADD COLUMN  "region" TEXT,
 ADD COLUMN  "total_prs" INTEGER,
 ADD COLUMN  "statusId" INTEGER,
-ADD COLUMN  "updated_at" TIMESTAMP(3)
+ADD COLUMN  "updated_at" TIMESTAMP(3),
+DROP COLUMN "id",
+ADD COLUMN  "id" INTEGER NOT NULL,
 ADD CONSTRAINT "Repository_pkey" PRIMARY KEY ("id");
 
 -- AlterTable renaming
@@ -33,12 +30,17 @@ ALTER TABLE "Repository" RENAME COLUMN "name" TO "repo_name";
 ALTER TABLE "Repository" RENAME COLUMN "owner" TO "repo_owner";
 ALTER TABLE "Repository" RENAME COLUMN "github_url" TO "repo_url";
 
+
 -- CreateTable
 CREATE TABLE "Status" (
     "id" SERIAL NOT NULL,
     "status" TEXT NOT NULL,
+
     CONSTRAINT "Status_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Repository_repo_name_key" ON "Repository"("repo_name");
 
 -- AddForeignKey
 ALTER TABLE "Repository" ADD CONSTRAINT "Repository_statusId_fkey" FOREIGN KEY ("statusId") REFERENCES "Status"("id") ON DELETE SET NULL ON UPDATE CASCADE;
