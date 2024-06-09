269eba76e2e832ce1e3d0754b0880da47687dce7
Mo Mahboobian <momahboobian@gmail.com>
2024-06-09 20:27:40 +0100

    Update and add table to confirm the data

--- app/updatedb/page.js
+++ app/updatedb/page.js
@@ -5,18 +5,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
+import ToggleCheckbox from "@/components/UpdateDB/ToggleCheckbox";
+
 export default function UpdateDB() {
   const [sheetId, setSheetId] = useState("");
+  const [action, setAction] = useState("update");
+  const [message, setMessage] = useState(null);
+  const [data, setData] = useState([]);
 
   const handleUpdateData = async () => {
     try {
+      setMessage(null);
+
       if (!sheetId) {
-        alert("Please enter a Google Sheet ID");
+        setMessage("Please enter a Google Sheet ID");
         return;
       }
 
-      const url = `/api/update-db?sheetId=${encodeURIComponent(sheetId)}`;
-
+      const url = `/api/update-db?sheetId=${encodeURIComponent(
+        sheetId
+      )}&action=${action}`;
       const response = await fetch(url, {
         method: "POST",
         headers: {
@@ -25,21 +33,30 @@ export default function UpdateDB() {
       });
 
       if (response.ok) {
-        const data = await response.json();
-        alert(data.message);
+        const responseData = await response.json();
+        setData(responseData.data);
+        setMessage(responseData.message);
       } else {
-        console.error("Error updating data:", response.statusText);
-        alert("Incorrect Google Sheet ID");
+        const errorMessage = await response.json();
+        console.error("Error response:", errorMessage);
+        setMessage(errorMessage.error);
+
+        setData([]);
       }
     } catch (error) {
-      console.error("Error updating data:", error);
-      alert("An error occurred. Please try again.");
+      console.error("Error fetching data:", error);
+      setMessage("An error occurred. Please try again.");
+      setData([]);
     }
   };
 
+  const toggleAction = () => {
+    setAction((prevAction) => (prevAction === "update" ? "remove" : "update"));
+  };
+
   return (
-    <div className="flex flex-col justify-center items-start h-screen w-full bg-[#070E0E]">
-      <div className="flex items-start justify-between px-6 md:pt-6">
+    <div className="flex flex-col justify-center items-start h-screen w-full bg-[#070E0E] p-4 pt-24 overflow-scroll md:pt-0 sm:h-screen">
+      <div className="flex items-start justify-between px-2 pt-6 md:pt-6">
         <div className="flex flex-col justify-between gap-4 py-2">
           <div className="flex items-center">
             <FontAwesomeIcon
@@ -56,30 +73,87 @@ export default function UpdateDB() {
           </p>
         </div>
       </div>
-      <div className="flex items-start justify-start h-full pt-10">
-        <div className="flex items-center gap-10">
-          <div className="relative focus-within:text-teal-700 ">
-            <span className="absolute inset-y-0 left-0 flex items-center ">
-              <FontAwesomeIcon
-                icon={faSearch}
-                className="h-5 pl-4 text-teal-700 "
+      <div className="flex flex-col items-center justify-start w-full h-full gap-10 pt-20 sm:items-start sm:pl-6 md:flex-row">
+        <div className="flex flex-col items-start justify-center gap-4">
+          <div className="flex flex-col items-start justify-center">
+            <div className="relative focus-within:text-teal-700 ">
+              <span className="absolute inset-y-0 left-0 flex items-center ">
+                <FontAwesomeIcon
+                  icon={faSearch}
+                  className="h-5 pl-4 text-teal-700 "
+                />
+              </span>
+              <input
+                type="text"
+                value={sheetId}
+                onChange={(e) => setSheetId(e.target.value)}
+                placeholder="Enter Google Sheet ID"
+                className="p-3 pl-12 bg-[#edeaea] font-medium rounded-md w-80 my-2 text-left text-md text-teal-700"
               />
-            </span>
-            <input
-              type="text"
-              value={sheetId}
-              onChange={(e) => setSheetId(e.target.value)}
-              placeholder="Enter Google Sheet ID"
-              className="p-3 pl-12 bg-[#edeaea] font-medium rounded-md md:w-80 my-[10%] text-left text-md text-teal-700"
-            />
+            </div>
+
+            {message && (
+              <div
+                className={`ml-2 text-s w-80 font-light text-${
+                  message.includes("successfully") ? "green" : "red"
+                }-500`}
+              >
+                {message}
+              </div>
+            )}
           </div>
+
+          <ToggleCheckbox
+            Remove
+            Existing
+            Data
+            message={"Remove Existing Data"}
+            filterActive={action !== "update"}
+            onToggle={toggleAction}
+          />
+
           <button
             onClick={handleUpdateData}
-            className="bg-[#37BCBA] text-gray-900 rounded-lg p-3 px-16 font-semibold flex items-center hover:bg-[#1a9997]"
+            className="bg-[#37BCBA] text-gray-900 rounded-lg p-3 px-28 font-semibold flex items-center hover:bg-[#1a9997] whitespace-nowrap"
           >
             Update Data
           </button>
         </div>
+
+        {data.length > 0 && (
+          <div className="mt-2 overflow-hidden shadow w-96 sm:rounded-lg">
+            <table className="min-w-full leading-normal bg-[#1e1e1e] text-sm text-gray-400">
+              <thead className="text-xs font-medium uppercase bg-gray-800">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-4 tracking-wider text-left"
+                  >
+                    Team Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-4 tracking-wider text-left"
+                  >
+                    Cohort
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item) => (
+                  <tr key={item.id}>
+                    <td className="w-1/2 px-6 py-4 border-b border-gray-700 whitespace-nowrap">
+                      {item.team_name}
+                    </td>
+                    <td className="w-1/2 px-6 py-4 border-b border-gray-700">
+                      {item.cohort}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
       </div>
     </div>
   );
