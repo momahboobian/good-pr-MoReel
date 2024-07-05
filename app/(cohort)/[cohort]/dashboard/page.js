fb420e726ef644547c1507ef88dec2bd5133913d
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-07-05 12:50:21 +0100

    cohort/dashboard routing

---
+++ app/(cohort)/[cohort]/dashboard/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import Dashboard from "@components/Dashboard/Dashboard";
+
+export default function page() {
+  return (
+    <div className="flex justify-center items-start bg-[#070E0E] h-full md:w-screen">
+      <Dashboard />
+    </div>
+  );
+}
