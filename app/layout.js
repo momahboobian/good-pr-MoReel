c7db329ee242d71af6ceb3d6650bd49e26eb5c07
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-07-15 12:43:47 +0100

    add head to the layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "@styles/globals.css";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={`${inter.className} h-full`}>{children}</body>
     </html>
   );
