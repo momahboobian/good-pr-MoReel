1a1b6f08da2952799c807274e763a199d3acd905
Mo Mahboobian <momahboobian@gmail.com>
2024-09-20 14:55:05 +0100

    updating backgroud colour

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
-import GroupsPage from "@components/GroupsPage/GroupsPage";
+import GroupsPage from '@components/GroupsPage/GroupsPage'
 
 export default function Home() {
   return (
-    <div className="flex items-start justify-center min-h-screen md:w-screen">
+    <div className="flex items-start justify-center min-h-screen bg-[#070E0E] md:w-screen">
       <GroupsPage />
     </div>
-  );
+  )
 }
