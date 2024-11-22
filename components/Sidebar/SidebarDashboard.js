4c0316494cd13bba9e4578183afdbbb61b4c5c20
Mo Mahboobian <momahboobian@gmail.com>
2024-11-22 15:36:47 +0000

    Chore: Fix lint issue covert Tooltips to array functions

--- components/Sidebar/SidebarDashboard.js
+++ components/Sidebar/SidebarDashboard.js
@@ -13,7 +13,7 @@ export default function SidebarDashboard() {
 
   return (
     <ul
-      className={`flex flex-col md:flex-row xl:flex-col justify-start items-start gap-6 xl:pt-20 whitespace-nowrap`}
+      className={"flex flex-col md:flex-row xl:flex-col justify-start items-start gap-6 xl:pt-20 whitespace-nowrap"}
     >
       <li className="flex items-center">
         <Link href="/" className="flex items-center">
