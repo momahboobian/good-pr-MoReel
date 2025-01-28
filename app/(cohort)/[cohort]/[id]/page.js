415c85175d8705c8a705da47a1507ec1f4e3a76b
Ben Gladman <ben.gladman@gmail.com>
2025-01-28 15:36:17 +0000

    chore: upgrade NextJS 15.1.6

--- app/(cohort)/[cohort]/[id]/page.js
+++ app/(cohort)/[cohort]/[id]/page.js
@@ -1,6 +1,7 @@
 import Dashboard from "@components/Dashboard/Dashboard";
 
-export default function page({ params }) {
+export default async function page(props) {
+  const params = await props.params;
   const { id } = params;
   return (
     <div className="flex justify-center items-start bg-[#070E0E] h-full">
--- app/(cohort)/[cohort]/page.js
+++ app/(cohort)/[cohort]/page.js
@@ -1,6 +1,7 @@
 import GroupsPage from "@components/GroupsPage/GroupsPage";
 
-export default function Cohort({ params }) {
+export default async function Cohort(props) {
+  const params = await props.params;
   const { cohort } = params;
   return <GroupsPage cohort={cohort} />;
 }
